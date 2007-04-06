c7f6affe7bf8ab29118bb3795568e7e8b3b5920b
Ilya WingedFox Lebedev <ilya@lebedev.net>
2007-04-06 11:40:00 +0000

    % changed code order, due to Opera's new behavior: scripts added via document.write are loaded synchronously % added ; between new VirtualKeyboard and function in round brackets, because Opera recognize it as parameters for new operator, ignoring 'VirtualSemicolon' in the JS specification.
    git-svn-id: http://svn.debugger.ru/repos/jslibs/Virtual%20Keyboard/trunk@252 9a90dfcf-8112-0410-8cf2-8049563067c9

--- vk_loader.js
+++ vk_loader.js
@@ -1,3 +1,13 @@
+VirtualKeyboard = new function () {
+  var self = this, to = null;
+  self.show = self.hide = self.toggle = self.attachInput = function () {
+     window.status = 'VirtualKeyboard is not loaded yet.';
+     if (!to) setTimeout(function(){window.status = ''},1000);
+  }
+  self.isOpen = function () {
+      return false;
+  }
+};
 (function () {
     var p = (function (sname){var sc=document.getElementsByTagName('script'),sr=new RegExp('^(.*/|)('+sname+')([#?]|$)');for (var i=0,scL=sc.length; i<scL; i++) {var m = String(sc[i].src).match(sr);if (m) {if (m[1].match(/^((https?|file)\:\/{2,}|\w:[\\])/)) return m[1];if (m[1].indexOf("/")==0) return m[1];b = document.getElementsByTagName('base');if (b[0] && b[0].href) return b[0].href+m[1];return (document.location.href.match(/(.*[\/\\])/)[0]+m[1]).replace(/^\/+/,"");}}return null;})('vk_loader.js');
     var pq = function (q) {if ('string'!=typeof q || q.length<2) return {};q = q.split("&");for (var z=0,qL=q.length,rs={},kv,rkv;z<qL;z++){kv=q[z].split("=");kv[0]=kv[0].replace(/[{}\[\]]*$/,"");rkv = rs[kv[0]];kv[1]=unescape(kv[1]?kv[1].replace("+"," "):"");if (rkv)if ('array'==typeof(rkv))rs[kv[0]][rs[kv[0]].length]=kv[1];else rs[kv[0]]=[rs[kv[0]],kv[1]];else rs[kv[0]]=kv[1];}return rs}
@@ -27,13 +37,3 @@
         document.write('<scr'+'ipt type="text/javascript" src="'+p+dpd[i]+'"></scr'+'ipt>');
     }
 })();
-VirtualKeyboard = new function () {
-  var self = this, to = null;
-  self.show = self.hide = self.toggle = self.attachInput = function () {
-     window.status = 'VirtualKeyboard is not loaded yet.';
-     if (!to) setTimeout(function(){window.status = ''},1000);
-  }
-  self.isOpen = function () {
-      return false;
-  }
-}
