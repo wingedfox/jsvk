e3c49c04488cf57cc91799a394a2a1a179820709
Ilya WingedFox Lebedev <ilya@lebedev.net>
2009-03-15 09:36:47 +0000

    - duplicate Czech layout
    git-svn-id: http://svn.debugger.ru/repos/jslibs/Virtual%20Keyboard/trunk@574 9a90dfcf-8112-0410-8cf2-8049563067c9

--- layouts/layouts.js
+++ layouts/layouts.js
@@ -1643,13 +1643,6 @@ zuo: "作做座左坐昨佐祚鑿怍椊葃阼稓柞酢夎胙繓筰葄嘬捽琢�
 ,shift:{0:'°1234567890%ˇ\'',24:'/(',35:'"!',44:'?:_'}
 ,alt:{1:'~ˇ^˘°˛`˙´˝¨¸¤\\|€',24:'÷×',27:'đĐ[]',33:'łŁ$ß',38:'#&@{}',44:'<>*'}
 ,dk:{'ˇ':'nňcčdďsšlľeěrřtťzžNŇCČDĎSŠLĽEĚRŘTŤZŽ ˇ','^':'aâeêuûiîoôAÂEÊUÛIÎOÔ ^','˘':'aăgğAĂGĞ ˘','°':'aåuůAÅUŮ °','˛':'aąeęuųiįAĄEĘUŲIĮ ˛','`':'aàeèuùiìoòAÀEÈUÙIÌOÒ `','˙':'eėiızżEĖIİZŻ ·','´':'nńcćyýaásślĺeérŕuúiízźoóNŃCĆYÝAÁSŚLĹEÉRŔUÚIÍZŹOÓ ´','˝':'uűoőUŰOŐ ˝','¨':'yÿaäeëuüiïoöYŸAÄEËUÜIÏOÖ ¨','¸':'nņcçgģsşlļkķrŗtţNŅCÇGĢSŞLĻKĶRŖTŢ ¸'}},
-{code:'CS-CZ'
-,name:'Czech'
-,normal:';+ěščřžýáíé=´¨qwertzuiopú)asdfghjklů§yxcvbnm,.-'
-,shift:{0:'°1234567890%ˇ\'',24:'/(',35:'"!',44:'?:_'}
-,alt:{1:'~ˇ^˘°˛`˙´˝¨¸¤\\|€',24:'÷×',27:'đĐ[]',33:'łŁ$ß',38:'#&@{}',44:'<>*'}
-,shift_caps:{2:'r'}
-,dk:{'ˇ':'nňcčdďsšlľeěrřtťzžNŇCČDĎSŠLĽEĚRŘTŤZŽ ˇ','^':'aâeêuûiîoôAÂEÊUÛIÎOÔ ^','˘':'aăgğAĂGĞ ˘','°':'aåuůAÅUŮ °','˛':'aąeęuųiįAĄEĘUŲIĮ ˛','`':'aàeèuùiìoòAÀEÈUÙIÌOÒ `','˙':'eėiızżEĖIİZŻ ·','´':'nńcćyýaásślĺeérŕuúiízźoóNŃCĆYÝAÁSŚLĹEÉRŔUÚIÍZŹOÓ ´','˝':'uűoőUŰOŐ ˝','¨':'yÿaäeëuüiïoöYŸAÄEËUÜIÏOÖ ¨','¸':'nņcçgģsşlļkķrŗtţNŅCÇGĢSŞLĻKĶRŖTŢ ¸'}},
 {code:'DA-DK'
 ,name:'Danish'
 ,normal:'½1234567890+´\'qwertyuiopå¨asdfghjklæøzxcvbnm,.-'
