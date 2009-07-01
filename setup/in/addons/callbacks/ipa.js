35df9900318ebc1feed80d849de457a74830b37c
Ilya WingedFox Lebedev <ilya@lebedev.net>
2009-07-01 09:33:03 +0000

    + IPA phonetic layout, http://en.wikipedia.org/wiki/International_Phonetic_Alphabet
    git-svn-id: http://svn.debugger.ru/repos/jslibs/Virtual%20Keyboard/trunk@640 9a90dfcf-8112-0410-8cf2-8049563067c9

---
+++ setup/in/addons/callbacks/ipa.js
@@ -0,0 +1,13 @@
+/**
+ * $Id$
+ *
+ * IPA char processor
+ *
+ * This software is protected by patent No.2009611147 issued on 20.02.2009 by Russian Federal Service for Intellectual Property Patents and Trademarks.
+ *
+ * @author Konstantin Wiolowan
+ * @copyright 2009 Konstantin Wiolowan <wiolowan@mail.ru>
+ * @version $Rev$
+ * @lastchange $Author$ $Date$
+ */
+VirtualKeyboard.Langs.IPA.charProcessor
